1bb9c976d9e09bb4eae37438840e6a0629691f07
Daniel Kogan <danielkoganx@gmail.com>
2022-02-18 17:22:39 -0500

    embed format

--- bot.py
+++ bot.py
@@ -14,5 +14,30 @@ intents = discord.Intents.all()
 load_dotenv()
 TOKEN = os.environ.get('TOKEN', 3)
 
-bot = commands.Bot(command_prefix=">", intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+example_dict = { # this is the object i need returned
+    "code": "CSE114",
+    "title": "Introduction to Object-Oriented Programming",
+    "description": "An introduction to procedural and object-oriented programming methodology. Topics include program structure, conditional and iterative programming, procedures, arrays and records, object classes, encapsulation, information hiding, inheritance, polymorphism, file I/O, and exceptions. Includes required laboratory. This course has been designated as a High Demand/Controlled Access (HD/CA) course. Students registering for HD/CA courses for the first time will have priority to do so.",
+    "prereq": "Level 5 or higher on the math placement exam",
+    "SBC": "TECH",
+    "credits": "4 credits"
+}
+
+def create_embed(query_dict):
+    embed = discord.Embed(
+        title=f"{query_dict["title"]}", description=f"{query_dict["description"]}", colour="#650100")
+    embed.add_field(name="Prerequisites",
+                    value=f"{query_dict["prereq"]}", inline=False)
+    embed.add_field(name="SBC",
+                    value=f"{query_dict["credits"]}", inline=False)
+    embed.add_field(name="Course Code",
+                    value=f"{query_dict["code"]}", inline=False)
+    return embed
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
 
