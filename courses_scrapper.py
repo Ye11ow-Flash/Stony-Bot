6a80f5d030c05c5fb9f0c8b5ea55bbd776d45109
Jaineel Shah <janshah@cs.stonybrook.edu>
2022-02-18 19:20:48 -0500

    Course Code added

--- courses_scrapper.py
+++ courses_scrapper.py
@@ -1,24 +1,22 @@
-from webbrowser import get
 import requests
 from bs4 import BeautifulSoup
 import sys
 import collections
 
 major = "bio"
-def get_courses(major):
-  url = "https://www.stonybrook.edu/sb/bulletin/current/courses/"+major+"/"
+url = "https://www.stonybrook.edu/sb/bulletin/current/courses/"+major+"/"
+def get_courses(url):
   source_code = requests.get(url)
   plain_text=source_code.text
   soup = BeautifulSoup(plain_text, features="html.parser")
   data = []
   for link in soup.findAll('div',{'class' : 'course'}):
-    #course_data = collections.defaultdict(str)
+    course_data = collections.defaultdict(str)
     title = link.find('h3').text
-    #course_data["title"] = 
-    data.append(title.split(":")[1])
+    course_data["code"] = title.split(":")[0].split("\xa0")[1]
+    course_data["title"] = title.split(":")[1]
+    data.append(course_data)
   # for i in data:
   #   print(i)
-  return data
 
-print(get_courses(major))
-
+get_courses(url)
